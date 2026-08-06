88d712e4c346f2fa712bbaaa503b4fd8833aa8ac
Vimukthi Kothalawala <86369500+VimukthiKothalawala@users.noreply.github.com>
2026-08-06 18:50:39 +0530

    Fixed proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const PUBLIC_PATHS = ["/sign-in", "/sign-up"];
+const PUBLIC_PATHS = ["/sign-in", "/sign-up", "/__clerk"];
 
 export default clerkMiddleware(async (auth, req) => {
   const { pathname } = req.nextUrl;
